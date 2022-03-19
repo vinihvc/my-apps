e3f776bffe490131287ca7058941ccd250bb078e
Vinicius Vicentini <viniciusvicentini@live.com>
2022-03-19 15:55:51 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# Installation
+
+Run the `apps.ps1` script to install the packages.
+
+# Requirements
+
+- `winget` support.
+
+### Dev
+
+- Windows Terminal
+- Visual Studio Code
+- Git
+- Ubuntu 20.04
+
+### Browser
+
+- Mozilla Firefox
+
+### Tools
+
+- Power Toys
+
+### Drivers
+
+- GeForce Experience
+
+### Comunication
+
+- Discord
+
+### Tools
+
+- Authy
+- Dropbox
+
+# More
+
+To include new packages, add them to the `apps.ps1` script.
+
+To know the package name, visit [winget.run](https://winget.run/).
---
+++ apps.ps1
@@ -0,0 +1,40 @@
+param([switch]$Elevated)
+
+# Ask for Administrator rights
+function Test-Admin {
+    $currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
+    $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+}
+
+if ((Test-Admin) -eq $false)  {
+    if ($elevated) {
+        # tried to elevate, did not work, aborting
+    } else {
+        Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile -noexit -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition))
+    }
+    exit
+}
+
+$myApps = @(
+# Dev
+'Microsoft.WindowsTerminal',
+'Microsoft.VisualStudioCode',
+'Git.Git',
+
+# WSL
+'Canonical.Ubuntu.2004',
+
+# Browser
+'Mozilla.Firefox',
+
+# Tools System
+'Microsoft.PowerToys',
+'Nvidia.GeForceExperience',
+'Twilio.Authy',
+'Dropbox.Dropbox',
+
+# Comunication
+'Discord.Discord'
+)
+
+$myApps | ForEach-Object -Process { winget install -e --id $_ }
